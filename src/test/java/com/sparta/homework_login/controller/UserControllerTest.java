6b5c88f34c3c2b74c4b1316c476800e4a6c1478e
guguggagga <gaeul79_@naver.com>
2025-02-13 19:02:39 +0900

    test: 로그인 컨트롤러 테스트 코드 작성 - 목객체를 활용하여 로그인 컨트롤러 테스트 코드 구현 - Mock객체 WithCustomMockUser 추가 - Mock객체 WithCustomMockUserSecurityContextFactory 추가

--- src/test/java/com/sparta/homework_login/controller/UserControllerTest.java
+++ src/test/java/com/sparta/homework_login/controller/UserControllerTest.java
@@ -1,16 +1,20 @@
 package com.sparta.homework_login.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.homework_login.dto.request.SignInRequestDto;
 import com.sparta.homework_login.dto.request.SignUpRequestDto;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.sparta.homework_login.entity.User;
+import com.sparta.homework_login.repository.UserRepository;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -21,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) // static 없이 @BeforeAll사용 가능
 public class UserControllerTest {
 
     @Autowired
@@ -29,19 +34,27 @@ public class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    private SignUpRequestDto createSignUpRequestDto(String username, String password, String nickname) {
-        return SignUpRequestDto.builder()
-                .username(username)
-                .password(password)
-                .nickname(nickname)
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void createUser() {
+        userRepository.deleteAll();
+        User user = User.builder()
+                .username("Hong")
+                .password(passwordEncoder.encode("1q2w3e4r#"))
+                .nickname("동에 번쩍")
                 .build();
+        userRepository.save(user);
     }
 
     @Test
     @DisplayName("회원가입 성공")
     public void signUp_success() throws Exception {
         // given
-        SignUpRequestDto requestDto = createSignUpRequestDto(
+        SignUpRequestDto requestDto = new SignUpRequestDto(
                 "Hong",
                 "1q2w3e4r#",
                 "동에 번쩍"
@@ -62,7 +75,7 @@ public class UserControllerTest {
     @DisplayName("회원가입 실패 - 잘못된 값 입력")
     public void signUp_failure_badInput() throws Exception {
         // given
-        SignUpRequestDto requestDto = createSignUpRequestDto(
+        SignUpRequestDto requestDto = new SignUpRequestDto(
                 "Hong",
                 "1q2w3e4r#",
                 null
@@ -83,13 +96,13 @@ public class UserControllerTest {
     @DisplayName("회원가입 실패 - 중복된 회원")
     public void signUp_failure_duplication() throws Exception {
         // given
-        SignUpRequestDto requestDto1 = createSignUpRequestDto(
+        SignUpRequestDto requestDto1 = new SignUpRequestDto(
                 "Hong",
                 "1q2w3e4r#",
                 "동에 번쩍"
         );
 
-        SignUpRequestDto requestDto2 = createSignUpRequestDto(
+        SignUpRequestDto requestDto2 = new SignUpRequestDto(
                 "Hong",
                 "1q2w3e4r#",
                 "서에 번쩍"
@@ -111,5 +124,71 @@ public class UserControllerTest {
                 .andExpect(status().isCreated());
         actions2.andDo(print())
                 .andExpect(status().isConflict());
+    }
+
+    @Test
+    @AutoConfigureMockMvc
+    @DisplayName("로그인 성공")
+    public void signIn_success() throws Exception {
+        // given
+        createUser();
+        SignInRequestDto requestDto = new SignInRequestDto(
+                "Hong",
+                "1q2w3e4r#"
+        );
+
+        // when
+        ResultActions actions = mockMvc.perform(post("/api/auth/login")
+                .content(objectMapper.writeValueAsString(requestDto))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        actions.andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @AutoConfigureMockMvc
+    @DisplayName("로그인 실패 - 존재하지 않는 유저")
+    public void signIn_failure_notMatchUsername() throws Exception {
+        // given
+        createUser();
+        SignInRequestDto requestDto = new SignInRequestDto(
+                "Kim",
+                "1q2w3e4r#"
+        );
+
+        // when
+        ResultActions actions = mockMvc.perform(post("/api/auth/login")
+                .content(objectMapper.writeValueAsString(requestDto))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        actions.andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @AutoConfigureMockMvc
+    @DisplayName("로그인 실패 - 비밀번호 불일치")
+    public void signIn_failure_notMatchPassword() throws Exception {
+        // given
+        createUser();
+        SignInRequestDto requestDto = new SignInRequestDto(
+                "Hong",
+                "1q2w3e4r"
+        );
+
+        // when
+        ResultActions actions = mockMvc.perform(post("/api/auth/login")
+                .content(objectMapper.writeValueAsString(requestDto))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        actions.andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
     }
 }
---
+++ src/test/java/com/sparta/homework_login/mock/WithCustomMockUser.java
@@ -0,0 +1,14 @@
+package com.sparta.homework_login.mock;
+
+import com.sparta.homework_login.dummy.WithCustomMockUserSecurityContextFactory;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithCustomMockUserSecurityContextFactory.class)
+public @interface WithCustomMockUser {
+
+    String username() default "Hong";
+}
